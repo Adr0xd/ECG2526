aee6d9a3ef3717805d45219eb2e6a70bc386319a
Mihai Diaconu <adimamihai@gmail.com>
2026-05-07 09:31:39 +0300

    egc lab 10

--- Lab2/bresenham.cpp
+++ Lab2/bresenham.cpp
@@ -1,84 +1,170 @@
 #include "bresenham.h"
 
 //Detects in which octant a line is situated
-int getOctant(BresenhamLine line) {
+int getOctant(const BresenhamLine line) {
+    const int dx = static_cast<int>(line.endX - line.startX);
+    const int dy = static_cast<int>(line.endY - line.startY); // Note: In SDL, dy > 0 means moving DOWN
 
-	int deltaY = static_cast<int>(line.endY - line.startY);
-	int deltaX = static_cast<int>(line.endX - line.startX);
-
-	//Slope		
-	float m = abs(deltaY) / (float)abs(deltaX);
-
-	if ((m < 1) && (deltaX > 0) && (deltaY <= 0)) //first octant
-		return 1;
-
-	return 0;
+    if (dx >= 0 && dy <= 0) {
+        return abs(dx) >= abs(dy) ? 1 : 2;
+    }
+    if (dx < 0 && dy <= 0) {
+        return abs(dy) >= abs(dx) ? 3 : 4;
+    }
+    if (dx < 0 && dy > 0) {
+        return abs(dx) >= abs(dy) ? 5 : 6;
+    }
+    return abs(dy) >= abs(dx) ? 7 : 8;
 }
 
 //Draws a new raster line using Bresenham's algorithm
-void BresenhamDraw(BresenhamLine line, SDL_Renderer* renderer) {
-	int tmpStartX = 0;
-	int tmpStartY = 0;
-	int tmpEndX = 0;
-	int tmpEndY = 0;
-	int tmpCurrentX = 0;
-	int tmpCurrentY = 0;
+void BresenhamDraw(const BresenhamLine line, SDL_Renderer *renderer) {
+    const int dx = abs(line.endX - line.startX);
+    const int dy = abs(line.endY - line.startY);
+    int x = line.startX;
+    int y = line.startY;
 
-	int dy = static_cast<int>(fabs(line.endY - line.startY));
-	int dx = static_cast<int>(fabs(line.endX - line.startX));
+    int d, inc1, inc2;
+    const int octant = getOctant(line);
 
-	int d, inc1, inc2;
+    switch (octant) {
+        case 1:
+            d = 2 * dy - dx;
+            inc1 = 2 * dy;
+            inc2 = 2 * (dy - dx);
+            for (; x <= line.endX; x++) {
+                SDL_RenderPoint(renderer, x, y);
+                if (d > 0) {
+                    y--;
+                    d += inc2;
+                } else { d += inc1; }
+            }
+            break;
 
-	int octant = getOctant(line);
+        case 2:
+            d = 2 * dx - dy;
+            inc1 = 2 * dx;
+            inc2 = 2 * (dx - dy);
+            for (; y >= line.endY; y--) {
+                SDL_RenderPoint(renderer, x, y);
+                if (d > 0) {
+                    x++;
+                    d += inc2;
+                } else { d += inc1; }
+            }
+            break;
 
-	switch (octant)
-	{
-	case 1:
-		tmpStartX = tmpCurrentX = static_cast<int>(line.startX);
-		tmpEndX = static_cast<int>(line.endX);
-		tmpStartY = tmpCurrentY = static_cast<int>(line.startY);
-		tmpEndY = static_cast<int>(line.endY);
+        case 3:
+            d = 2 * dx - dy;
+            inc1 = 2 * dx;
+            inc2 = 2 * (dx - dy);
+            for (; y >= line.endY; y--) {
+                SDL_RenderPoint(renderer, x, y);
+                if (d > 0) {
+                    x--;
+                    d += inc2;
+                } else { d += inc1; }
+            }
+            break;
 
-		d = 2 * dy - dx;
-		inc1 = 2 * dy;
-		inc2 = 2 * (dy - dx);
+        case 4:
+            d = 2 * dy - dx;
+            inc1 = 2 * dy;
+            inc2 = 2 * (dy - dx);
+            for (; x >= line.endX; x--) {
+                SDL_RenderPoint(renderer, x, y);
+                if (d > 0) {
+                    y--;
+                    d += inc2;
+                } else { d += inc1; }
+            }
+            break;
 
-		while (tmpCurrentX < tmpEndX)
-		{
-			//Draw current point
-			SDL_RenderPoint(renderer, static_cast<float>(tmpCurrentX), static_cast<float>(tmpCurrentY));
-			++tmpCurrentX;
+        case 5:
+            d = 2 * dy - dx;
+            inc1 = 2 * dy;
+            inc2 = 2 * (dy - dx);
+            for (; x >= line.endX; x--) {
+                SDL_RenderPoint(renderer, x, y);
+                if (d > 0) {
+                    y++;
+                    d += inc2;
+                } else { d += inc1; }
+            }
+            break;
 
-			if (d < 0)
-				d += inc1;
-			else
-			{
-				d += inc2;
-				--tmpCurrentY;
-			}
-		}
-		break;
-	case 2:
-		break;
-	case 3:
-		break;
-	case 4:
-		break;
-	case 5:
-		break;
-	case 6:
-		break;
-	case 7:
-		break;
-	case 8:
-		break;
+        case 6:
+            d = 2 * dx - dy;
+            inc1 = 2 * dx;
+            inc2 = 2 * (dx - dy);
+            for (; y <= line.endY; y++) {
+                SDL_RenderPoint(renderer, x, y);
+                if (d > 0) {
+                    x--;
+                    d += inc2;
+                } else { d += inc1; }
+            }
+            break;
 
-	default:
-		break;
-	}
+        case 7:
+            d = 2 * dx - dy;
+            inc1 = 2 * dx;
+            inc2 = 2 * (dx - dy);
+            for (; y <= line.endY; y++) {
+                SDL_RenderPoint(renderer, x, y);
+                if (d > 0) {
+                    x++;
+                    d += inc2;
+                } else { d += inc1; }
+            }
+            break;
+
+        case 8:
+            d = 2 * dy - dx;
+            inc1 = 2 * dy;
+            inc2 = 2 * (dy - dx);
+            for (; x <= line.endX; x++) {
+                SDL_RenderPoint(renderer, x, y);
+                if (d > 0) {
+                    y++;
+                    d += inc2;
+                } else { d += inc1; }
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+void renderPoints(SDL_Renderer* renderer, const int x, const int y, const int cx, const int cy) {
+    SDL_RenderPoint(renderer, static_cast<float>(cx + x), static_cast<float>(cy + y));
+    SDL_RenderPoint(renderer, static_cast<float>(cx + x), static_cast<float>(cy - y));
+    SDL_RenderPoint(renderer, static_cast<float>(cx - x), static_cast<float>(cy + y));
+    SDL_RenderPoint(renderer, static_cast<float>(cx - x), static_cast<float>(cy - y));
+    SDL_RenderPoint(renderer, static_cast<float>(cx + y), static_cast<float>(cy + x));
+    SDL_RenderPoint(renderer, static_cast<float>(cx - y), static_cast<float>(cy + x));
+    SDL_RenderPoint(renderer, static_cast<float>(cx + y), static_cast<float>(cy - x));
+    SDL_RenderPoint(renderer, static_cast<float>(cx - y), static_cast<float>(cy - x));
 }
 
 //Draws a new raster circle using Bresenham's algorithm
-void BresenhamDraw(BresenhamCircle circle, SDL_Renderer* renderer) {
+void BresenhamDraw(const BresenhamCircle circle, SDL_Renderer* renderer) {
+    int currentY = circle.radius;
 
+    float d = 0.25f - circle.radius;
+
+    const int end = static_cast<int>(ceil(circle.radius / sqrt(2.0)));
+
+    for (int x = 0; x <= end; x++) {
+        renderPoints(renderer, x, currentY, circle.centerX, circle.centerY);
+
+        d += 2 * x + 1;
+
+        if (d > 0) {
+            d += 2 - (2 * currentY);
+            currentY--;
+        }
+    }
 }
+
+
--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -1,51 +1,32 @@
-﻿//  SDL_TestClip
+// EGC_Bresenham.cpp : Defines the entry point for the console application.
 //
-//  Created by CGIS on 16/02/2025.
-//  Copyright � 2025 CGIS. All rights reserved.
+//  Created by CGIS on 13/04/2020.
+//  Copyright ? 2020 CGIS. All rights reserved.
 //
-//  This is a test setup for your clipping algorithm implementation
-//  This application draws the clipping window beteen the points (250,200) and (550, 400)
-//  You can draw the line that you want to clip with a "drag-n-drop" action within the window
-//
-//  To apply the Cyrus-Beck algorithm to your line, press the 'c' key.
-//  This will invoke the function implementation from the "clip.cpp" file. 
-//  You must implement the functions within that file!!!! 
-//  Check "clip.h" and the Laboratory work for clues about the implemntation
-//
-
-/************************************************* IMPORTANT *********************************************************/
-//  !!!!! ADD ALL OF YOUR IMPLEMENTED SOURCES -> vec3, vec4, mat3, mat4, transform (".h" and ".cpp") files !!!!!!
-//
-/*******************************************************************************************************************/
 
 #include <iostream>
 #include <SDL3/SDL.h>
-#include "clip.h"
-
-using namespace egc;
-
-// define rectangle vertices
-vec3 p1 { 0.0f, 0.0f, 1.0f };
-vec3 p2 { 0.0f, 0.0f, 1.0f };
-
-//define clipping window
-std::vector<vec3> clipWindow;
+#include "bresenham.h"
 
 //define window dimensions
-constexpr int WINDOW_WIDTH = 800;
-constexpr int WINDOW_HEIGHT = 600;
+constexpr int WINDOW_WIDTH { 640 };
+constexpr int WINDOW_HEIGHT { 480 };
 
+//The window
 SDL_Window* window { nullptr };
+//The window renderer
 SDL_Renderer* renderer { nullptr };
 SDL_Event currentEvent;
 
-SDL_Color backgroundColor { 255,255,255,255 };
-SDL_Color clippingWindowColor { 0, 128, 0, 255 };
-SDL_Color lineColor { 0, 0, 255,255 };
+SDL_Color backgroundColor { 255, 255, 255, 255 };
+SDL_Color lineColor { 0, 0, 0, 255 };
+SDL_Color circleColor { 0, 255, 255, 255 };
 
 bool quit { false };
-
 float mouseX { -1.0f }, mouseY { -1.0f };
+
+BresenhamLine myLine;
+BresenhamCircle myCircle;
 
 float displayScale { 1.0f };
 
@@ -64,11 +45,11 @@ bool initWindow()
 		displayScale = SDL_GetDisplayContentScale(1);
 
 		if (!SDL_CreateWindowAndRenderer(
-			"SDL Hello World Example",
-			static_cast<int>(displayScale * WINDOW_WIDTH),
-			static_cast<int>(displayScale * WINDOW_HEIGHT),
-			SDL_WINDOW_HIGH_PIXEL_DENSITY,
-			&window, &renderer))
+				"SDL Hello World Example",
+				static_cast<int>(displayScale * WINDOW_WIDTH),
+				static_cast<int>(displayScale * WINDOW_HEIGHT),
+				SDL_WINDOW_HIGH_PIXEL_DENSITY,
+				&window, &renderer))
 		{
 			SDL_Log("Failed to create window and renderer: %s\n", SDL_GetError());
 			success = false;
@@ -105,27 +86,79 @@ void destroyWindow()
 	SDL_Quit();
 }
 
-//Create the corners of the clipping window (drawn as a rectangle)
-void initClipWindow()
+void handleMouseEvents()
 {
-	clipWindow.push_back(vec3(250.0f, 200.0f, 1.0f));
-	clipWindow.push_back(vec3(550.0f, 200.0f, 1.0f));
-	clipWindow.push_back(vec3(550.0f, 400.0f, 1.0f));
-	clipWindow.push_back(vec3(250.0f, 400.0f, 1.0f));
+	//Mouse event -> pressed button
+	if (currentEvent.type == SDL_EVENT_MOUSE_BUTTON_DOWN)
+	{
+		if (currentEvent.button.button == SDL_BUTTON_LEFT)
+		{
+			SDL_GetMouseState(&mouseX, &mouseY);
+			mouseX /= displayScale;
+			mouseY /= displayScale;
+			myLine.startX = myLine.endX = mouseX;
+			myLine.startY = myLine.endY = mouseY;
+		}
+
+		if (currentEvent.button.button == SDL_BUTTON_RIGHT)
+		{
+			SDL_GetMouseState(&mouseX, &mouseY);
+			mouseX /= displayScale;
+			mouseY /= displayScale;
+			myCircle.centerX = mouseX;
+			myCircle.centerY = mouseY;
+			myCircle.radius = 0.0f;
+		}
+	}
+
+	//Mouse event -> mouse movement
+	if (currentEvent.type == SDL_EVENT_MOUSE_MOTION)
+	{
+		SDL_MouseButtonFlags mouseButtons = SDL_GetMouseState(nullptr, nullptr);
+		if (mouseButtons & SDL_BUTTON_MASK(SDL_BUTTON_LEFT))
+		{
+			SDL_GetMouseState(&mouseX, &mouseY);
+			mouseX /= displayScale;
+			mouseY /= displayScale;
+			myLine.endX = mouseX;
+			myLine.endY = mouseY;
+		}
+
+		if (mouseButtons & SDL_BUTTON_MASK(SDL_BUTTON_RIGHT))
+		{
+			SDL_GetMouseState(&mouseX, &mouseY);
+			mouseX /= displayScale;
+			mouseY /= displayScale;
+			myCircle.radius = sqrt((mouseX - myCircle.centerX) * (mouseX - myCircle.centerX) +
+				(mouseY - myCircle.centerY) * (mouseY - myCircle.centerY));
+		}
+	}
 }
 
-int main(int argc, char * argv[]) {
+void handleKeyboardEvents()
+{
+	//Keyboard event
+	if (currentEvent.type == SDL_EVENT_KEY_DOWN)
+	{
+		switch (currentEvent.key.key)
+		{
+		case SDLK_ESCAPE:
+			quit = true;
+			break;
 
+		default:
+			break;
+		}
+	}
+}
+
+
+int main(int argc, char * argv[]) {
 	if (!initWindow())
 	{
 		SDL_Log("Failed to initialize");
 		return -1;
 	}
-
-	initClipWindow();
-
-	std::cout << "Draw the line you want to clip with a drag-n-drop mouse action!" << "\n";
-	std::cout << "Press the \'c\' key to apply the Cyrus-Beck clipping algorithm!" << "\n\n";
 
 	SDL_zero(currentEvent);
 
@@ -139,71 +172,22 @@ int main(int argc, char * argv[]) {
 				quit = true;
 			}
 
-			//Mouse event -> pressed button
-			if (currentEvent.type == SDL_EVENT_MOUSE_BUTTON_DOWN)
-			{
-				if (currentEvent.button.button == SDL_BUTTON_LEFT)
-				{
-					//left mouse button was pressed
-					SDL_GetMouseState(&mouseX, &mouseY);
-					mouseX /= displayScale;
-					mouseY /= displayScale;
-					p1.x = p2.x = mouseX;
-					p1.y = p2.y = mouseY;
-				}
-			}
+			handleMouseEvents();
+			handleKeyboardEvents();
 
-			//Mouse event -> mouse movement
-			if (currentEvent.type == SDL_EVENT_MOUSE_MOTION)
-			{
-				SDL_MouseButtonFlags mouseButtons = SDL_GetMouseState(nullptr, nullptr);
-				if (mouseButtons & SDL_BUTTON_MASK(SDL_BUTTON_LEFT))
-				{
-					//left button pressed while moving
-					SDL_GetMouseState(&mouseX, &mouseY);
-					mouseX /= displayScale;
-					mouseY /= displayScale;
-					p2.x = mouseX;
-					p2.y = mouseY;
-				}
-			}
-
-			//Keyboard event
-			if (currentEvent.type == SDL_EVENT_KEY_DOWN)
-			{
-				switch (currentEvent.key.key)
-				{
-				case SDLK_C:
-					std::cout << "Applying Cyrus-Beck clipping" << "\n";
-					//Applies the Cyrus-Beck clipping algorithm -> implemented by you
-					if (lineClip_CyrusBeck(clipWindow, p1, p2) == -1)
-						p1.x = p2.x = p1.y = p2.y = 0.0f;
-					break;
-				case SDLK_ESCAPE:
-					quit = true;
-					break;
-				default:
-					break;
-				}
-			}
-			//Paint the white background
+			//Clear screen
 			SDL_SetRenderDrawColor(renderer, backgroundColor.r, backgroundColor.g, backgroundColor.b, backgroundColor.a);
 			SDL_RenderClear(renderer);
 
-			//draw the clipping window
-			SDL_SetRenderDrawColor(renderer, clippingWindowColor.r, clippingWindowColor.g, clippingWindowColor.b, clippingWindowColor.a);
-			for (size_t i = 0; i < clipWindow.size(); i++)
-				SDL_RenderLine(
-					renderer,
-					clipWindow.at(i).x,
-					clipWindow.at(i).y,
-					clipWindow.at((i + 1) % clipWindow.size()).x,
-					clipWindow.at((i + 1) % clipWindow.size()).y);
-
-			//draw the line
+			//Draw Bresenham line
 			SDL_SetRenderDrawColor(renderer, lineColor.r, lineColor.g, lineColor.b, lineColor.a);
-			SDL_RenderLine(renderer, p1.x, p1.y, p2.x, p2.y);
+			BresenhamDraw(myLine, renderer);
 
+			//Draw Bresenham circle
+			SDL_SetRenderDrawColor(renderer, circleColor.r, circleColor.g, circleColor.b, circleColor.a);
+			BresenhamDraw(myCircle, renderer);
+
+			//Update screen
 			SDL_RenderPresent(renderer);
 		}
 	}
